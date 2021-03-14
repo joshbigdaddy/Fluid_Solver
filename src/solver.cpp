017a39412327a15ecb744391032d44b479312dfe
Joshbigdaddy <joshua.cifuentes.romero@hotmail.com>
2021-03-14 18:21:19 +0100

    Upload of solver code

---
+++ src/solver.cpp
@@ -0,0 +1,293 @@
+#include "solver.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void Solver::Init(unsigned N, float dt, float diff, float visc)
+{
+	this->dt = dt;
+	this->diff = diff;
+	this->visc = visc;
+	this->N = N;
+	this->size = (N + 2) * (N + 2);
+}
+/*
+----------------------------------------------------------------------
+free/clear/allocate simulation data
+----------------------------------------------------------------------
+*/
+void Solver::FreeData(void)
+{
+//TODO: Libera los buffers de memoria.
+	if(u) free(u);
+	if(v) free(v);
+	if (u_prev) free(u_prev);
+	if (v_prev) free(v_prev);
+	if (dens) free(dens);
+	if (dens_prev) free(dens_prev);
+
+
+}
+
+void Solver::ClearData(void)
+{
+//TODO: Borra todo el contenido de los buffers
+	for (int i = 0; i < size;i++) {
+		u[i] = 0.0f;
+		u_prev[i] = 0.0f;
+		v[i] = 0.0f;
+		v_prev[i] = 0.0f;
+		dens[i] = 0.0f;
+		dens_prev[i] = 0.0f;
+	}
+}
+
+bool Solver::AllocateData(void)
+{
+//TODO:
+//Reservamos memoria, en caso de fallo devlvemos false.
+//Antes de devolver true, hay que limpiar la memoria reservada con un ClearData().
+	u = (float*)malloc(size*sizeof(float));
+	if (!u) return false;
+	v = (float*)malloc(size * sizeof(float));
+	if (!v) return false;
+	u_prev = (float*)malloc(size * sizeof(float));
+	if (!u_prev) return false;
+	v_prev = (float*)malloc(size * sizeof(float));
+	if (!v_prev) return false;
+	dens = (float*)malloc(size * sizeof(float));
+	if (!dens) return false;
+	dens_prev = (float*)malloc(size * sizeof(float));
+	if (!dens_prev) return false;
+	ClearData();
+	return true;
+}
+
+void Solver::ClearPrevData() 
+{
+//TODO: Borra el contenido de los buffers _prev
+	for (int i = 0; i < size; i++) {
+		u_prev[i] = 0.0f;
+		v_prev[i] = 0.0f;
+		dens_prev[i] = 0.0f;
+	}
+}
+
+void Solver::AddDensity(unsigned x, unsigned y, float source)
+{
+//TODO: Añade el valor de source al array de densidades. Sería interesante usar la macro: XY_TO_ARRAY
+	dens_prev[XY_TO_ARRAY(x, y)]+=source;
+}
+
+void Solver::AddVelocity(unsigned x, unsigned y, float forceX, float forceY)
+{
+//TODO: Añade el valor de fuerza a sus respectivos arrays. Sería interesante usar la macro: XY_TO_ARRAY
+	u_prev[XY_TO_ARRAY(x, y)]+=forceX;
+	v_prev[XY_TO_ARRAY(x, y)]+= forceY;
+
+}
+
+void Solver::Solve()
+{
+	VelStep();
+	DensStep();
+}
+
+void Solver::DensStep()
+{
+	AddSource(dens, dens_prev);			//Adding input density (dens_prev) to final density (dens).
+	SWAP(dens_prev, dens)				//Swapping matrixes, because we want save the next result in dens, not in dens_prev.
+	Diffuse(0, dens, dens_prev);		//Writing result in dens because we made the swap before. bi = dens_prev. The initial trash in dens matrix, doesnt matter, because it converges anyways.
+	SWAP(dens_prev, dens);				//Swapping matrixes, because we want save the next result in dens, not in dens_prev.
+	Advect(0, dens, dens_prev, u, v);	//Advect phase, result in dens.
+}
+
+void Solver::VelStep()
+{
+	AddSource(u, u_prev);
+	AddSource(v, v_prev);
+	SWAP (u_prev,u)			
+	SWAP (v_prev, v)
+	Diffuse(1, u, u_prev);  
+	Diffuse(2, v, v_prev); 
+	Project(u, v, u_prev, v_prev);		//Mass conserving.
+	SWAP(u_prev, u);
+	SWAP(v_prev, v);
+	Advect(1, u, u_prev, u_prev, v_prev);
+	Advect(2, v, v_prev, u_prev, v_prev);
+	Project(u, v, u_prev, v_prev);		//Mass conserving.
+}
+
+void Solver::AddSource(float * base, float * source)
+{
+//TODO: Teniendo en cuenta dt (Delta Time), incrementar el array base con nuestro source. Esto sirve tanto para añadir las nuevas densidades como las nuevas fuerzas.
+	for (int i = 0; i < size;i++) {
+		base[i] = base[i] + (source[i] * dt);
+	}
+}
+
+
+void Solver::SetBounds(int b, float * x)
+{
+/*TODO:
+Input b: 0, 1 or 2.
+	0: borders = same value than the inner value.
+	1: x axis borders inverted, y axis equal.
+	2: y axis borders inverted, x axis equal.
+	Corner values allways are mean value between associated edges.
+*/
+	//vamos de 0 a n+1
+	//tanto en x como en y
+	if (b == 0) {
+		//borde x sup
+		for (unsigned j = 1; j <= N; j++) {
+			//bordes x
+			x[XY_TO_ARRAY(j, 0)] = x[XY_TO_ARRAY(j, 1)];
+			x[XY_TO_ARRAY(j, N+1)] = x[XY_TO_ARRAY(j, N)];
+
+			//bordes y
+			x[XY_TO_ARRAY(0, j)] = x[XY_TO_ARRAY(1, j)];
+			x[XY_TO_ARRAY(N+1, j)] = x[XY_TO_ARRAY(N, j)];
+		}
+
+	}
+	if (b == 1) {
+
+		for (unsigned j = 1; j <= N; j++) {
+			x[XY_TO_ARRAY(j, 0)] = x[XY_TO_ARRAY(j, 0)]*-1;
+			x[XY_TO_ARRAY(j, N+1)] = x[XY_TO_ARRAY(j, N+1)]*-1;
+		}
+
+	}
+	if (b == 2) {
+
+		for (unsigned j = 1; j <= N; j++) {
+
+			x[XY_TO_ARRAY(0, j)] = x[XY_TO_ARRAY(0, j)]*-1;
+			x[XY_TO_ARRAY(N+1, j)] = x[XY_TO_ARRAY(N+1, j)]*-1;
+		}
+	}
+	x[XY_TO_ARRAY(0, 0)] = (x[XY_TO_ARRAY(1, 0)] + x[XY_TO_ARRAY(0, 1)]) / 2;
+	x[XY_TO_ARRAY(0, N + 1)] = (x[XY_TO_ARRAY(0, N)] + x[XY_TO_ARRAY(1, N+1)]) / 2;
+	x[XY_TO_ARRAY(N + 1, 0)] = (x[XY_TO_ARRAY(N, 1)] + x[XY_TO_ARRAY(N + 1, 1)]) / 2;
+	x[XY_TO_ARRAY(N + 1, N + 1)] = (x[XY_TO_ARRAY(N, N + 1)] + x[XY_TO_ARRAY(N + 1, N)] ) / 2;
+}
+
+/*
+https://www.youtube.com/watch?v=62_RUX_hrT4
+https://es.wikipedia.org/wiki/M%C3%A9todo_de_Gauss-Seidel <- Solución de valores independientes.
+Despreciando posibles valores de x no contiguos, se simplifica mucho. Mirar diapositivas y la solución de Gauss Seidel de términos independientes.
+Gauss Seidel -> Matrix x and x0
+*/
+void Solver::LinSolve(int b, float * x, float * x0, float aij, float aii)
+{
+//TODO: Se recomienda usar FOR_EACH_CELL, END_FOR y XY_TO_ARRAY.
+	int i;
+	int j;
+	for (int it = 0; it < 20; it++) {
+		SetBounds(b, x);
+
+			FOR_EACH_CELL
+
+				x[XY_TO_ARRAY(i, j)] = ((aij)*
+				 (x[XY_TO_ARRAY(i - 1, j)] + x[XY_TO_ARRAY(i, j - 1)]
+						+ x[XY_TO_ARRAY(i + 1, j)] + x[XY_TO_ARRAY(i, j + 1)])+x0[XY_TO_ARRAY(i, j)]) / aii;
+			
+			END_FOR;
+		
+			
+	}
+}
+
+/*
+Nuestra función de difusión solo debe resolver el sistema de ecuaciones simplificado a las celdas contiguas de la casilla que queremos resolver,
+por lo que solo con la entrada de dos valores, debemos poder obtener el resultado.
+*/
+void Solver::Diffuse(int b, float * x, float * x0)
+{
+//TODO: Solo necesitaremos pasar dos parámetros a nuestro resolutor de sistemas de ecuaciones de Gauss Seidel. Calculamos dichos valores y llamamos a la resolución del sistema.
+	//k es difusion
+	float k;
+	if (b == 0) {
+		//densidad --> diff
+		k = diff;
+	}
+	else {
+	//velocidades--> visc
+		k = visc;
+	}
+	float h =   (N*N);
+	float aij = (k * dt)*h;
+	float aii = 1 + (4 * aij);
+	LinSolve(b,x,x0,aij,aii);
+	
+}
+
+/*
+d is overwrited with the initial d0 data and affected by the u & v vectorfield.
+Hay que tener en cuenta que el centro de las casillas representa la posición entera dentro de la casilla, por lo que los bordes estan
+en las posiciones x,5.
+*/
+void Solver::Advect(int b, float * d, float * d0, float * u, float * v)
+{
+//TODO: Se aplica el campo vectorial realizando una interploación lineal entre las 4 casillas más cercanas donde caiga el nuevo valor.
+
+
+	int i, j;
+	FOR_EACH_CELL
+	SetBounds(b, d);
+	float x_0 = i - (u[XY_TO_ARRAY(i, j)] * dt*N);
+	float y_0 = j - (v[XY_TO_ARRAY(i, j)] * dt*N);
+
+	//x_0 e y_0 vienen decimales, hay que buscar las casillas
+	if (x_0 < 0) {
+		x_0 = 0.5;
+	}
+	if (x_0 > N) {
+		x_0 = N - 0.5;
+	}
+	if (y_0 < 0) {
+		y_0 = 0.5;
+	}
+	if (y_0 > N) {
+		y_0 = N - 0.5;
+	}
+	int x_0_INT=x_0, y_0_INT=y_0;
+	float s = x_0 - x_0_INT;
+	float t = y_0 - y_0_INT;
+
+	//Ahora tenemos la primera casilla (la que le corresponde
+
+	d[XY_TO_ARRAY(i, j)] = ((d0[XY_TO_ARRAY(x_0_INT, y_0_INT)] * (1-s)*(1-t)))+
+							(d0[XY_TO_ARRAY(x_0_INT +1, y_0_INT)] * (s)*(1-t)) +
+							(d0[XY_TO_ARRAY(x_0_INT, y_0_INT +1)] *(1-s)*(t))+
+							(d0[XY_TO_ARRAY(x_0_INT +1, y_0_INT + 1)])*(s)*(t);
+
+		END_FOR;
+		
+}
+
+/*
+Se encarga de estabilizar el fluido y hacerlo conservativo de masa. Se usa solo en las matrices de velocidades.
+No necesaria implementación por su complejidad.
+*/
+void Solver::Project(float * u, float * v, float * p, float * div)
+{
+	int i, j;
+
+	FOR_EACH_CELL
+		div[XY_TO_ARRAY(i, j)] = -0.5f*(u[XY_TO_ARRAY(i + 1, j)] - u[XY_TO_ARRAY(i - 1, j)] + v[XY_TO_ARRAY(i, j + 1)] - v[XY_TO_ARRAY(i, j - 1)]) / N;
+		p[XY_TO_ARRAY(i, j)] = 0;
+	END_FOR
+	SetBounds(0, div);
+	SetBounds(0, p);
+
+	LinSolve(0, p, div, 1, 4);
+	//habria que poner un linsolve2 por un trigger en el teclado o algo para lo del gradiente conjugado
+	//Aproximamos: Laplaciano de q a su gradiente.
+	FOR_EACH_CELL
+		u[XY_TO_ARRAY(i, j)] -= 0.5f*N*(p[XY_TO_ARRAY(i + 1, j)] - p[XY_TO_ARRAY(i - 1, j)]);
+		v[XY_TO_ARRAY(i, j)] -= 0.5f*N*(p[XY_TO_ARRAY(i, j + 1)] - p[XY_TO_ARRAY(i, j - 1)]);
+	END_FOR
+	SetBounds(1, u);
+	SetBounds(2, v);
+}
